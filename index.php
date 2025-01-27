b2346b8c5f0bae47381fba72efbdc3752a3dee36
Ali Mansour <ali.a.mansour2004@gmail.com>
2025-01-28 01:53:28 +0200

    adding faker library

--- index.php
+++ index.php
@@ -1,11 +1,19 @@
 <?php
 
 require 'Carbon/autoload.php';
+require 'Faker/src/autoload.php';
 
 use Carbon\Carbon;
+use Faker\Factory;
 
 $time = "2021-08-04 16:19:49";
 Carbon::setLocale('ar');
 $dt = Carbon::createFromFormat('Y-m-d H:i:s', $time);
 
-echo $dt->diffForHumans() . "\n";
+echo $dt->diffForHumans() . "<br>";
+
+$faker = Factory::create();
+
+echo "Name: " . $faker->name . "<br>";
+echo "Address: " . $faker->address . "<br>";
+echo "Phone: " . $faker->phoneNumber . "<br>";
